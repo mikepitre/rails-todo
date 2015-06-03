527367a3af55f9538c835e568e548f7dde355afc
Mike Pitre <michaelepitre@gmail.com>
2015-06-03 14:34:45 -0500

    added render json to view all todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,9 +3,9 @@ class TodosController < ApplicationController
   def index
     all_todos = Todo.all
     display_todos = all_todos.map do |todo|
-      "#{todo.id}) #{todo.body}, #{todo.completed}"
+      "#{todo.id}) #{todo.body} --- #{todo.completed}"
     end
-    render text: display_todos.join
+    render json: Todo.all
   end
 
 end
