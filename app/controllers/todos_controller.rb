dc3c9744c0e2abf7d9f500a18764217b70fac303
Mike Pitre <michaelepitre@gmail.com>
2015-06-03 14:35:41 -0500

    got rid of unnecessary index code in todos controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,10 +1,6 @@
 class TodosController < ApplicationController
 
   def index
-    all_todos = Todo.all
-    display_todos = all_todos.map do |todo|
-      "#{todo.id}) #{todo.body} --- #{todo.completed}"
-    end
     render json: Todo.all
   end
 
